494e4eab83f6f0e423689011613d5876e0d10841
Gabriel Jordan <gjordanp@gmail.com>
2023-02-07 11:50:01 -0300

    second

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    //React Fragment
+    <div className='container'> 
+      <h1>Hola Mundo</h1>
+      <h2>Hola ...</h2>
+    </div>
+  )
+}
+
+export default App
