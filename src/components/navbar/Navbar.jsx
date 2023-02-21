2148d130ffdca3e1a2390174fd6bc02fc44d79d7
Gabriel Jordan <gjordanp@gmail.com>
2023-02-20 23:26:32 -0300

    logo added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
-import {ListH, ListV} from '../lists/List'
+import { ListH, ListV } from '../lists/List'
 import CartWidget from './CartWidget'
 import styles from './navbar.module.css'
+import flykiteLogo from '../../assets/flykite-logo.svg'
 
 
 const NavBar = (props) => {
     return (
         <div className={styles.container}>
-            <h2>{props.title}</h2>
-            <ListH list = {["Velas","Barras","Tablas","Accesorios"]} handleClick={props.handleClick}></ListH>
-            <CartWidget notifications="3" />
+            <div className={styles.logoContainer}>
+                <img src={flykiteLogo} width="100" height="100" alt="" />
+                <h2>{props.title}</h2>
+            </div>
+            <ListH list={["Velas", "Barras", "Tablas", "Accesorios", <CartWidget notifications="3" />]} handleClick={props.handleClick}></ListH>
+            {/* <CartWidget notifications="3" /> */}
         </div>
     )
 }
