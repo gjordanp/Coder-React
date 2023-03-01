7ae3c6c05c90b72841e343bcf0578633e171e8bc
Gabriel Jordan <gjordanp@gmail.com>
2023-03-01 01:21:20 -0300

    navbar responsive

---
+++ src/components/menu/TemporaryDrawer.jsx
@@ -0,0 +1,85 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import { useState, useEffect } from 'react';
+
+export default function TemporaryDrawer({anchor, open}) {
+  const [state, setState] = useState(false);
+  useEffect(() => {setState(open)}, [open])
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState(open);
+  };
+
+  const handleState = () => {open=!open};
+ const handleStateClose = () => {setState(false)};
+ const handleStateOpen = () => {setState(true)};
+
+  
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      role="presentation"
+      onClick={handleState}
+      onKeyDown={handleState}
+    // onClick={handleState}
+    // onKeyDown={handleState}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <div>
+        <React.Fragment key={anchor}>
+          {/* <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button> */}
+          
+          <Drawer
+            anchor={anchor}
+            open={open}
+            // onClose={toggleDrawer(anchor, false)}
+            onClose={handleState}
+          >
+            {list(anchor)}
+          </Drawer>
+        </React.Fragment>
+    </div>
+  );
+}
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,20 +3,28 @@ import { ListH, ListV } from '../lists/List'
 import CartWidget from './CartWidget'
 import styles from './navbar.module.css'
 import flykiteLogo from '../../assets/flykite-logo.svg'
+import useMediaQuery from '@mui/material/useMediaQuery';
+import ResponsiveAppBar from './ResponsiveAppBar'
 
-const categories=["Velas", "Barras", "Tablas", "Accesorios", <CartWidget notifications="3" />];
 
-const NavBar = ({title,setBtnText,setShowAlert}) => {
-    return (
-        <nav className={styles.container}>
+const categories = ["Velas", "Barras", "Tablas", "Accesorios", <CartWidget notifications="3" />];
+
+
+const NavBar = ({ title, setBtnText, setShowAlert }) => {
+    return(
+    <>
+            <nav className={styles.container}>
             <div className={styles.logoContainer}>
-                <img src={flykiteLogo} width="80" height="80" alt="" />
+                <img src={flykiteLogo} width="70" height="70" alt="" />
                 <h2>{title}</h2>
             </div>
             <ListH list={categories} setBtnText={setBtnText} setShowAlert={setShowAlert}></ListH>
             {/* <CartWidget notifications="3" /> */}
         </nav>
-    )
-}
+        <ResponsiveAppBar/>
+    </>
 
+    )
+
+}
 export default NavBar
